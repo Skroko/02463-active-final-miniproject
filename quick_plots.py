c0f47f50dc2798279c18bac86f5388d337ebf0b7
Skroko <73649109+Skroko@users.noreply.github.com>
2022-04-21 17:44:35 +0200

    Fast as fuck boiii

--- quick_plots.py
+++ quick_plots.py
@@ -1,19 +1,40 @@
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
+import pandas as pd
 
 #%%
-a = np.random.rand(200)*10
-b = np.sin(a)
-c = np.random.normal(loc = 0, scale=a)
-d = a**4
-e = 1/a
 
+data = pd.read_csv("Data/data_534.csv")
+N_vars = 4
+vars = ["A","B","C","D"]
 
-axs, fig = plt.subplots(2,2)
-y_axs = [b,c,d,e]
+#%%
+# Pairwise plots
+fig, axs = plt.subplots(4,4,figsize=(15,15))
+
+for i,axs_i in enumerate(axs):
+    for j,ax in enumerate(axs_i):
+        if i != j:
+            ax.scatter(data[vars[i]],data[vars[j]],linewidths =0.1)
+
+#%%
+# Normal hists
+fig, axs = plt.subplots(4,figsize=(15,15))
+vars = ["A","B","C","D"]
+print(type(axs))
 for i,ax in enumerate(axs):
-    ax
+    ax.hist(data[vars[i]])
 
-plt.scatter(a,f)
+
+#%%
+# Linear Correlation
+corre_matrix = np.zeros((N_vars,N_vars))
+for i in range(N_vars):
+    for j in range(N_vars):
+        corre_matrix[i,j] = np.round(np.corrcoef(data[vars[i]],data[vars[j]])[0,1],3)
+    
+
+corre_matrix
 
